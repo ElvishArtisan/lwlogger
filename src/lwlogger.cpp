3e3a9f6105b5ecfe49c85e083017be875bfb70d6
Fred Gleason <fredg@paraelsystems.com>
2016-12-26 13:38:38 -0500

    2016-12-26 Fred Gleason <fredg@paravelsystems.com> 	* Initial package creation.

---
+++ src/lwlogger.cpp
@@ -0,0 +1,38 @@
+// lwlogger.cpp
+//
+// lwlogger(8) LiveWire routing daemon
+//
+//   (C) Copyright 2016 Fred Gleason <fredg@paravelsystems.com>
+//
+//   This program is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as
+//   published by the Free Software Foundation; either version 2 of
+//   the License, or (at your option) any later version.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public
+//   License along with this program; if not, write to the Free Software
+//   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+
+#include <QCoreApplication>
+
+#include "lwlogger.h"
+
+MainObject::MainObject(QObject *parent)
+  : QObject(parent)
+{
+}
+
+
+int main(int argc,char *argv[])
+{
+  QCoreApplication a(argc,argv);
+  new MainObject();
+
+  return a.exec();
+}
